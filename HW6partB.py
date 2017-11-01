84077987b9c5b5fa6a9fe6e80783fde87b99fe1a
Jacob Kirsch <jbkirsch@umich.edu>
2017-11-01 18:16:25 -0400

    added hw7

--- HW6partB.py
+++ HW6partB.py
@@ -1,10 +1,3 @@
-# To run this, you can install BeautifulSoup
-# https://pypi.python.org/pypi/beautifulsoup4
-
-# Or download the file
-# http://www.py4e.com/code3/bs4.zip
-# and unzip it in the same directory as this file
-
 import urllib.request, urllib.parse, urllib.error
 from bs4 import BeautifulSoup
 import ssl
@@ -15,7 +8,7 @@ ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
 
 url = input('Enter URL: ')
-totalCount = int(input('Enter count:')) + 1
+totalCount = int(input('Enter count:'))
 finalPosition = int(input('Enter position: '))
 html = urllib.request.urlopen(url, context=ctx).read()
 soup = BeautifulSoup(html, 'html.parser')
